436674195d906384fab4dc13f3c1c7284ad54bf1
cpiggott <cpiggott@ksu.edu>
2014-02-27 20:57:57 -0600

    First Commit

---
+++ SteamGameSelector/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SteamGameSelector
+{
+    partial class RandomGameGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.selectFolder = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Location = new System.Drawing.Point(11, 96);
+            this.selectFolder.Name = "selectFolder";
+            this.selectFolder.Size = new System.Drawing.Size(316, 23);
+            this.selectFolder.TabIndex = 1;
+            this.selectFolder.Text = "Select Folder";
+            this.selectFolder.UseVisualStyleBackColor = true;
+            this.selectFolder.Click += new System.EventHandler(this.selectFolder_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Random Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please select the folder steamapps using the \'Select Folder\' button.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // RandomGameGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 171);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.selectFolder);
+            this.Controls.Add(this.textBox1);
+            this.Name = "RandomGameGenerator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button selectFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SteamGameSelector/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace SteamGameSelector
+{
+    public partial class RandomGameGenerator : Form
+    {
+
+        public RandomGameGenerator()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        List<string> idss = new List<string>();
+
+        private void selectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            //System.Environment.SpecialFolder root = new Environment.SpecialFolder();
+            fbd.SelectedPath = "C:\\Program Files (x86)\\Steam\\steamapps";
+            if (DialogResult.OK == fbd.ShowDialog())
+            {
+                string path = fbd.SelectedPath.ToString();
+                idss = getrandomfile2(path);
+                textBox1.Text = path;
+                button1.Enabled = true;
+                
+
+            }
+        }
+
+        public static List<string> getrandomfile2(string path)
+        {
+            string file = null;
+            List<string> appIDs = new List<string>();
+            StreamReader lookThrough;
+            if (!string.IsNullOrEmpty(path))
+            {
+                var extensions = new string[] { ".acf" };
+                try
+                {
+                    var di = new DirectoryInfo(path);
+                    var rgFiles = di.GetFiles("*.*").Where(f => extensions.Contains(f.Extension.ToLower()));
+                    //Random R = new Random();
+                    //file = rgFiles.ElementAt(R.Next(0, rgFiles.Count())).FullName;
+                    string tempLine = "";
+                    for (int i = 0; i < rgFiles.Count(); i++)
+                    {
+                        lookThrough = new StreamReader(rgFiles.ElementAt(i).FullName.ToString());
+                        while (!lookThrough.EndOfStream)
+                        {
+                            tempLine = lookThrough.ReadLine().ToLower();
+                            if (tempLine.Contains("appid"))
+                            {
+                                string[] splitdata = tempLine.Split('"');
+                                
+                                appIDs.Add(splitdata[3]);
+                                break;
+                            }
+
+                        }
+                        //store the appid in the array of appID's
+
+                    }
+
+
+                }
+                // probably should only catch specific exceptions
+                // throwable by the above methods.
+                catch { }
+            }
+            return appIDs;
+        }
+
+        private static void startApp(List<string> ids)
+        {
+            Random r = new Random();
+            int fileLoc = r.Next(0, ids.Count - 1);
+            string id = ids[fileLoc];
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "C:\\Program Files (x86)\\Steam\\steam.exe";
+            startInfo.Arguments = " -applaunch " + id;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startApp(idss);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
